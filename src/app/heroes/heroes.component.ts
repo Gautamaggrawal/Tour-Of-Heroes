985a6d33039364189b0008451ce5c2b7ab62593c
Gautam <gautamaggrawalsd@yahoo.in>
2019-04-20 03:03:41 +0530

    added 2 way databinding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  heroes = HEROES;
 
   constructor() { }
 
   ngOnInit() {
-
   }
 
 }
